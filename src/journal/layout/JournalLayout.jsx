28dac31a388dfab3d8a5193d3aa969d7b61b401d
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-04-27 12:34:14 -0500

    NavBar and SideBar components

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,20 +1,21 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, Toolbar } from '@mui/material'
+import { NavBar, SideBar } from '../components';
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex'}}>
         
-        {/* Navbar drawerWidth */}
+        <NavBar drawerWidth={drawerWidth} />
 
-        {/* Sidebar drawerWidth */}
+        <SideBar drawerWidth={drawerWidth} />
+
         <Box 
             component='main'
             sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3}}        
         >
-            {/* Toolbar */}
+            <Toolbar/>
 
             { children }
 
