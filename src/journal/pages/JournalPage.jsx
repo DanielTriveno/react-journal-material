ebe0b63231877def5757bf8d9f3760a7d4034854
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-04-27 11:20:32 -0500

    JournaLayout and JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,16 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant='h1'>JournalPage</Typography>    
-    </>
+    
+    <JournalLayout>
+      
+    <Typography>Minim officia amet reprehenderit laborum aute ut commodo amet mollit ad aliquip officia adipisicing.</Typography>    
+
+    { /* NothingSelected */}
+    { /* NotePage */ }
+
+    </JournalLayout>
   )
 }
