40f6898d8e37a1569b2969dff02eebd902061b08
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2024-04-28 00:56:58 -0500

    NothingSelectedView NoteView and ImageList

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,15 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     
     <JournalLayout>
       
-    <Typography>Minim officia amet reprehenderit laborum aute ut commodo amet mollit ad aliquip officia adipisicing.</Typography>    
+    {/*<Typography>Minim officia amet reprehenderit laborum aute ut commodo amet mollit ad aliquip officia adipisicing.</Typography>*/}    
 
-    { /* NothingSelected */}
-    { /* NotePage */ }
+    {/* <NothingSelectedView /> */}
+    <NoteView />
 
     </JournalLayout>
   )
